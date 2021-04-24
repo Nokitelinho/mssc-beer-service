bbff4387874e939611fc16e93ca638b8c73e0646
Litovchenko Igor <nokitelinho@gmail.com>
2021-04-24 11:55:45 +0300

    Adding bootstrap loader - closes #7

---
+++ src/main/java/nokitelinho/msscbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,45 @@
+package nokitelinho.msscbeerservice.bootstrap;
+
+import nokitelinho.msscbeerservice.web.domain.Beer;
+import nokitelinho.msscbeerservice.web.repository.BeerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class BeerLoader implements CommandLineRunner {
+    private final BeerRepository beerRepository;
+
+    @Autowired
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects() {
+        if (beerRepository.count() ==0) {
+            beerRepository.save(Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .minOnHand(12)
+                    .quantityToBrew(200)
+                    .upc(337010000001L)
+                    .price(new BigDecimal("12.95"))
+                    .build());
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
+                    .minOnHand(12)
+                    .quantityToBrew(200)
+                    .upc(337010000007L)
+                    .price(new BigDecimal("11.75"))
+                    .build());
+        }
+    }
+}
