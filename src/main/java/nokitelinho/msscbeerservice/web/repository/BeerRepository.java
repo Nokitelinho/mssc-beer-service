182cd64e6eca3ed87cfbab7efb452cbebc2670a4
Litovchenko Igor <nokitelinho@gmail.com>
2021-04-24 11:34:03 +0300

    Adding beer repository. Closes #6

---
+++ src/main/java/nokitelinho/msscbeerservice/web/repository/BeerRepository.java
@@ -0,0 +1,9 @@
+package nokitelinho.msscbeerservice.web.repository;
+
+import nokitelinho.msscbeerservice.web.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
